bd85138823b3bf6a845bde223f30a0ef31be8f9a
SERAFINO <ymontenegro@estudiantec.cr>
2025-02-09 01:54:01 -0600

    Router: Para redirecciones y declaración de rutas

--- skynirussboking/src/App.jsx
+++ skynirussboking/src/App.jsx
@@ -1,11 +1,15 @@
-import Principal from './components/Pages/Principal'
+// src/App.jsx
+import { Routes, Route } from "react-router-dom";
+import Principal from "./components/Pages/Principal";
+import Monitor from "./components/Pages/Monitor/Monitor";
 
 function App() {
   return (
-    <div>
-      <Principal />
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Principal />} />
+      <Route path="/monitor" element={<Monitor />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- skynirussboking/src/main.jsx
+++ skynirussboking/src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { PageContextProvider } from './context/ContextPageProvider.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = createRoot(document.getElementById('root'))
 
 root.render(
   <StrictMode>
     <PageContextProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </PageContextProvider>
   </StrictMode>,
 )
